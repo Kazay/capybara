7d1fa62e2fd2ae8706180ac23b89077c3db36b60
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2018-04-20 00:07:08 +0200

    swaped from vue to knockout

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,31 +7,13 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+window.ko = require('knockout');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import * as exampleComponent from './components/example-component';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-Vue.component(
-    'passport-clients',
-    require('./components/passport/Clients.vue')
-);
-
-Vue.component(
-    'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue')
-);
-
-Vue.component(
-    'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue')
-);
-
-const app = new Vue({
-    el: '#app'
+ko.components.register('example', {
+    viewModel: exampleComponent.ReservationsViewModel,
+    template: require('./components/example-component.html')
 });
+
+ko.applyBindings();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,10 +17,7 @@
                     You are logged in!
                 </div>
             </div>
-            
-            <passport-clients></passport-clients>
-            <passport-authorized-clients></passport-authorized-clients>
-            <passport-personal-access-tokens></passport-personal-access-tokens>
+            <example></example>
         </div>
     </div>
 </div>
